65dfe2ed3f8b553093539b256524f052ec16702e
ceico119 <kosyrew.egor@gmail.com>
2022-10-20 17:30:32 +0400

    Add files via upload

--- database.py
+++ database.py
@@ -1,17 +1,40 @@
-import sys
-import pyodbc 
-# Some other example server values are
-# server = 'localhost\sqlexpress' # for a named instance
-# server = 'myserver,port' # to specify an alternate port
-server = '85.236.170.148,444' 
-database = 'PythonBotPguty' 
-username = 'stud' 
-password = 'stud' 
-# ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
+import pymysql
+
+mySQLServer = 'localhost'
+myDataBase = 'PgutyBot'
+user = 'root'
+passwodr = ''
 try:
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
-    cursor = cnxn.cursor()
-except Exception:
-    print("Ошибка")
-    print(sys.exc_info()[2])
+    connection = pymysql.connect(host = mySQLServer,port = 3306,database = myDataBase,user = user,password = passwodr,cursorclass = pymysql.cursors.DictCursor)
+    print('succesfully conneted')
+
+    try:
+        with connection.cursor() as cursor:
+            select_name_grup = "SELECT * FROM Rating;"
+            cursor.execute(select_name_grup)
+            rows = cursor.fetchall()
+            all_name_group = [] # Список всех групп
+            for row in rows:
+                if row['idSubject'] == 2 and row['name_group']=='ИВТ26у':
+                    print(row['link_rating'])
+            
+            
+        with connection.cursor() as cursor:
+            select_name_grup = "SELECT * FROM Subject;"
+            cursor.execute(select_name_grup)
+            rows = cursor.fetchall()
+            name_subject = {}
+            for row in rows:
+                key, value = row['idSubject'], row['Subject']
+                name_subject[key] = value
+            print(name_subject)    
+               
+            
+    finally:
+        connection.close()
+
+
+except Exception as ex:
+    print('Connection refused')
+    print('ex')
 
---
+++ fgd.py
@@ -0,0 +1,207 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import pymysql
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, reply_keyboard
+from auth import TOKEN
+
+
+"""Информация для подлкючения к базам данных"""
+mySQLServer = 'localhost'
+myDataBase = 'PgutyBot'
+user = 'root'
+passwodr = ''
+
+
+"""Индификация бота"""
+bot = Bot(token=TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+"""Подключение к базе данных"""
+try:
+    connection = pymysql.connect(host = mySQLServer,port = 3306,database = myDataBase,user = user,password = passwodr,cursorclass = pymysql.cursors.DictCursor)
+    print('<<<<<< БАЗА ДАННЫХ УСПЕШНО РАБОТАЕТ >>>>>>') 
+        
+    """БОТ ТЕЛЕГРАМ"""
+    class InputData(StatesGroup):
+        input_group = State()
+        input_subject = State()
+        input_lab = State()
+        input_teacher_add = State()
+        input_teacher_chenge = State()
+
+    @dp.message_handler(commands=['start'])
+    async def send_welcome(message: types.Message):
+        kb = [
+            [types.KeyboardButton(text="Преподаватель")],
+            [types.KeyboardButton(text="Студент")]
+        ]
+        keyboardStart = types.ReplyKeyboardMarkup(
+            keyboard=kb,
+            resize_keyboard=True
+        )
+        await message.answer(f'Привет {message.from_user.full_name} 👋\nКем вы являетесь?', reply_markup=keyboardStart)
+
+    @dp.message_handler()
+    async def students(message: types.Message):
+        if message.text == "Студент":
+            markup = InlineKeyboardMarkup().add(InlineKeyboardButton("Успеваемость", callback_data="group_input")).add(InlineKeyboardButton("Практика", callback_data="lab_input"))
+            await message.answer("Выберет что вам нужно", reply_markup=markup)
+        if message.text == "Преподаватель":
+            markup = InlineKeyboardMarkup().add(InlineKeyboardButton("Посмотреть\изменить успеваемость", callback_data="group_input")).add(InlineKeyboardButton("Изменить лабораторную работу", callback_data="lab_input_teacher_change")).add(InlineKeyboardButton("Добавить лабораторную работу", callback_data="lab_input_teacher_add"))
+            await message.answer("Выберет что вам нужно", reply_markup=markup)
+
+    # 1 Call
+    @dp.callback_query_handler(text="group_input")
+    async def group_call(call: types.CallbackQuery):
+        await call.answer()
+        await call.message.answer("Напишите группу и предмет \n\nПример: Ивт26у Математика", reply_markup=types.ReplyKeyboardRemove())
+        await InputData.input_group.set()
+        
+    # 2 Call    
+    @dp.callback_query_handler(text="lab_input")
+    async def group_call(call: types.CallbackQuery):
+        await call.answer()
+        await call.message.answer("Напишите нужный вам предмет\nи название работы\nПример: Русский практика", reply_markup=types.ReplyKeyboardRemove())
+        await InputData.input_lab.set()
+        
+    # 3 Call
+    @dp.callback_query_handler(text="lab_input_teacher_add")
+    async def add_lab_call(call: types.CallbackQuery):
+        await call.answer()
+        await call.message.answer("Напишите нужный что вы хотите добавить\n\nПример: Математика, Решение массивных уравнений, практика, ссылка на задание", reply_markup=types.ReplyKeyboardRemove())
+        await InputData.input_teacher_add.set()
+    # 4 Call
+    @dp.callback_query_handler(text="lab_input_teacher_change")
+    async def add_lab_call(call: types.CallbackQuery):
+        await call.answer()
+        await call.message.answer("Напишите нужный что вы хотите изменить\n\nПример: Математика, Решение массивных уравнений, практика, ссылка на задание", reply_markup=types.ReplyKeyboardRemove())
+        await InputData.input_teacher_chenge.set()
+    # 1 Input    
+    @dp.message_handler(state=InputData.input_group)
+    async def group_input(message: types.Message):
+        group_subject_info = message.text.lower().split(" ")
+        
+        with connection.cursor() as cursor:
+            select_name_grup = "SELECT * FROM Subject;"
+            cursor.execute(select_name_grup)
+            rows = cursor.fetchall()            
+            name_subject = {}
+            for row in rows:
+                key, value = row['idSubject'], row['Subject']
+                name_subject[key] = value  
+        
+        for k, j in name_subject.items():
+            if j == group_subject_info[1]:
+                idsubject = k
+        
+        with connection.cursor() as cursor:
+            select_name_grup = "SELECT * FROM Rating;"
+            cursor.execute(select_name_grup)
+            rows = cursor.fetchall()            
+            name_subject = {}
+            for row in rows:
+                if row['idSubject']==idsubject and row['name_group']==str(group_subject_info[0]):
+                    link_rating = row['link_rating']
+                
+        await message.answer(f"Ваша группа - {group_subject_info[0]}\nВыбранный вами предмет - {group_subject_info[1]}\n\nВаша успеваемость доступна по ссылке: {link_rating}")
+    
+    # 2 Input    
+    @dp.message_handler(state=InputData.input_lab)
+    async def lab_input(message: types.Message):
+        subject_lab_info = message.text.lower().split(" ")
+        
+        with connection.cursor() as cursor:
+            select_name_grup = "SELECT * FROM Subject;"
+            cursor.execute(select_name_grup)
+            rows = cursor.fetchall()            
+            name_subject = {}
+            for row in rows:
+                key, value = row['idSubject'], row['Subject']
+                name_subject[key] = value  
+        
+        for k, j in name_subject.items():
+            if j == subject_lab_info[0]:
+                idsubject = k
+        
+        with connection.cursor() as cursor:
+            select_name_grup = "SELECT * FROM LabPractick;"
+            cursor.execute(select_name_grup)
+            rows = cursor.fetchall()            
+            dict_lab = {}
+            for row in rows:
+                if row['idSubject']==idsubject and row['LabOrPrack']==str(subject_lab_info[1]):
+                    key, value = row['namePractick'], row['linkPractick']
+                    dict_lab[key] = value
+                    
+        for k, j in dict_lab.items():
+            name_prack = k
+            linkPractick = j
+            await message.answer(f"Выбранный вами предмет - {subject_lab_info[0]}\n\nНазвание данной работы: {str(name_prack).title()}\n\n{subject_lab_info[1].title()} по ссылке: {linkPractick}")
+    
+    # 3 Input
+    @dp.message_handler(state=InputData.input_teacher_add)
+    async def add_lab_input(message: types.Message):
+        # Взяли данные и разделили по запятой
+        # 0-Математика 1-Решение массивных уравнений 2-практика 3-Ссылка
+        group_subject_info = message.text.lower().split(",")
+        with connection.cursor() as cursor:
+            select_name_grup = "SELECT * FROM Subject;"
+            cursor.execute(select_name_grup)
+            rows = cursor.fetchall()            
+            name_subject = {}
+            for row in rows:
+                key, value = row['idSubject'], row['Subject']
+                name_subject[key] = value  
+        
+        for k, j in name_subject.items():
+            if j == group_subject_info[0]:
+                idsubject = k
+               
+        with connection.cursor() as cursor:
+            add_record = f"INSERT INTO `LabPractick` (`idSubject`, `LabOrPrack`, `namePractick`, `linkPractick`) VALUES ('{idsubject}', '{group_subject_info[2]}', '{group_subject_info[1]}', '{group_subject_info[3]}');"
+            cursor.execute(add_record)
+            connection.commit()
+        await message.answer(f"Предмет - {group_subject_info[0]}\n\nНазвание работы - {group_subject_info[1]}\n\nЗначение работы - {group_subject_info[2]}\n\nСсылка на работу - {group_subject_info[3]}\n\nВы добавили новую работу для студента")
+    # input 4
+    @dp.message_handler(state=InputData.input_teacher_chenge)
+    async def change_lab(message: types.Message):
+        group_subject_info = message.text.lower().split(",")
+        # поиск по id предмета и по названию лаба1, лаба2...
+        # print(group_subject_info)
+        with connection.cursor() as cursor:
+            with connection.cursor() as cursor:
+                select_name_grup = "SELECT * FROM Subject;"
+                cursor.execute(select_name_grup)
+                rows = cursor.fetchall()            
+                name_subject = {}
+                for row in rows:
+                    key, value = row['idSubject'], row['Subject']
+                    name_subject[key] = value  
+        
+        for k, j in name_subject.items():
+            if j == group_subject_info[0]:
+                idsubject = k
+
+        with connection.cursor() as cursor:
+            delete_record = f"DELETE FROM `LabPractick` WHERE `idSubject` = {idsubject} and `namePractick` = {group_subject_info[1]};"
+            cursor.execute(delete_record)
+            connection.commit()
+            print('вы удалили хуйню')
+
+        
+
+
+
+    """Обработчик запуска бота"""   
+    if __name__ == '__main__':
+        executor.start_polling(dp, skip_updates=True)
+    
+#При возникновении ошибки к подключении к базе данных
+except Exception as ex:
+    print('Connection refused')
+    print('ex')     
+    
+
